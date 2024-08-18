1a8d0599ecf1bbaf4778229b07bc8b2d740b4836
Alimisolution <alimirosheed5000@gmail.com>
2024-08-18 22:33:12 +0100

    Added-Anination-to-Single-product

--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -4,6 +4,12 @@ import Loader from "../../components/Loader/Loader";
 import { useEffect, useState } from "react";
 import TypingEffect from "../../components/TypingEffect/TypingEffect";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init();
+
+
 function DetailsPage() {
   const {id}  = useParams();
 const [singleProduct, setSingleProduct ] = useState({})
@@ -50,16 +56,17 @@ useEffect(() =>{
       {isLoading ? <Loader/> : (   
         <div className="space-y-20 sm:text-center">
             <h1 className="font-semibold sm:text-3xl text-2xl text-center">{text}</h1>
-            <div className="flex flex-col items-center justify-center shadow-2xl py-10 px-3">
-            <img src={singleProduct.image} className="sm:w-[300px] block" alt={singleProduct.title} />
-            <div className="pt-4">
-            <p className="mb-3 font-semibold sm:text-2xl text-lg text-center">{singleProduct.title}</p>
-            <p className="text-sm sm:text-lg">{singleProduct.description}</p>
-            <div className="mt-3">
-            <p className="text-lg">{singleProduct.category}</p>
-            <p className="font-semibold text-lg">${singleProduct.price}</p>
-            </div>
-            </div>
+            <div className="flex flex-col items-center justify-center shadow-2xl py-10 px-3" data-aos="fade-up"
+            data-aos-duration="500">
+              <img src={singleProduct.image} className="sm:w-[300px] block" alt={singleProduct.title} />
+              <div className="pt-4">
+                <p className="mb-3 font-semibold sm:text-2xl text-lg text-center">{singleProduct.title}</p>
+                <p className="text-sm sm:text-lg">{singleProduct.description}</p>
+              <div className="mt-3">
+                <p className="text-lg">{singleProduct.category}</p>
+                <p className="font-semibold text-lg">${singleProduct.price}</p>
+              </div>
+              </div>
             </div>
         </div>
         )}
